05194d4c2831b7aafaa278a6c436536d1be487b9
mikee47 <mike@sillyhouse.net>
2024-07-28 21:37:40 +0100

    Load config into objects

--- tools/dbgen.py
+++ tools/dbgen.py
@@ -7,6 +7,7 @@ import argparse
 import os
 import sys
 import json
+from dataclasses import dataclass
 
 CPP_TYPENAMES = {
     'object': '-',
@@ -16,19 +17,64 @@ CPP_TYPENAMES = {
     'boolean': 'bool',
 }
 
-class JsonPath:
-    '''Manages node identifier path'''
-    def __init__(self, nodes: list, name: str):
-        self.name = name
-        self.nodes = nodes
+@dataclass
+class Object:
+    parent: 'Object'
+    name: str
+    properties: dict
+    children: list
+
+    @property
+    def typename(self):
+        return make_typename(self.name)
+
+    @property
+    def id(self):
+        return make_identifier(self.name)
+
+    @property
+    def store(self):
+        return self.parent.store
+
+    @property
+    def base_class(self):
+        return f'{self.store.namespace}::Object'
 
     @property
     def path(self):
-        return '.'.join(self.nodes)
+        return join_path(self.parent.path, self.name) if self.parent else self.name
+
+@dataclass
+class Store(Object):
+    namespace: str
 
     @property
-    def type_name(self):
-        return make_typename(self.name)
+    def store(self):
+        return self
+
+    @property
+    def path(self):
+        return ''
+
+    @property
+    def base_class(self):
+        return f'{self.namespace}::Store'
+
+@dataclass
+class Database(Object):
+    default_store: Store
+
+    @property
+    def store(self):
+        return self.default_store
+
+    @property
+    def path(self):
+        return ''
+
+    @property
+    def base_class(self):
+        return 'Database'
 
 
 def make_identifier(s: str, is_type: bool = False):
@@ -44,6 +90,12 @@ def make_identifier(s: str, is_type: bool = False):
     return id
 
 
+def join_path(path: str, name: str):
+    if path:
+        path += '.'
+    return path + name
+
+
 def make_typename(s: str):
     '''Form valid CamelCase type name'''
     return make_identifier(s, True)
@@ -54,108 +106,121 @@ def make_string(s: str):
     return f'_F("{s}")'
 
 
-def parse_object(location: JsonPath, store_kind: str, config: dict) -> list:
-    is_database = ('database' in config)
-    is_store = ('store' in config)
-    if is_database:
-        if not is_store:
-            raise RuntimeError(f'Database requires default "store" value')
-        store_kind = make_typename(config['store'])
-        baseClass = 'Database'
-    elif is_store:
-        store_kind = make_typename(config['store'])
-        baseClass = f'{store_kind}::Store'
-    else:
-        baseClass = f'{store_kind}::Object'
+def load_config(filename: str) -> Database:
+    '''Load JSON configuration schema and parse into python objects
+    '''
+    with open(filename, 'r') as f:
+        config = json.load(f)
 
-    if is_database:
-        location.nodes = []
+    dbname = os.path.splitext(os.path.basename(filename))[0]
+    db = Database(None, dbname, config['properties'], [], None)
+    store_ns = config.get('store')
+    if store_ns is None:
+        raise RuntimeError(f'Database requires default "store" value')
+    db.default_store = Store(db, 'defaultStore', [], None, make_typename(store_ns))
+    config['object'] = db
 
+    def parse(obj: Object, cfg: dict):
+        for key, value in cfg['properties'].items():
+            if value['type'] != 'object':
+                continue
+            properties = value['properties']
+            if store_ns := value.get('store'):
+                child_object = Store(obj, key, properties, [], make_typename(store_ns))
+            else:
+                child_object = Object(obj, key, properties, [])
+            obj.children.append(child_object)
+            parse(child_object, value)
+
+    parse(db, config)
+    return db
+
+
+def parse_object(obj: Object) -> list:
     output = [
         '',
-        f'class {location.type_name}: public ConfigDB::{baseClass}',
+        f'class {obj.typename}: public ConfigDB::{obj.base_class}',
         '{',
         'public:',
     ]
 
-    # Get list of any immediate child objects which we should instantiate
-    children = []
-    for key, value in config['properties'].items():
-        if value['type'] == 'object':
-            loc = JsonPath(location.nodes + [key], key)
-            output += [parse_object(loc, store_kind, value)]
-            children.append(key)
+    # Append child object definitions
+    output += [parse_object(child) for child in obj.children]
 
-    if is_database:
+    if isinstance(obj, Database):
         tmp = [f'Database(path)']
-        tmp += [f'{make_identifier(c)}(store)' for c in children]
+        for child in obj.children:
+            if isinstance(child, Store):
+                tmp += [f'{child.id}(*this)']
+            else:
+                tmp += [f'{child.id}(*this)']
         init_list = [f'{x},' for x in tmp[:-1]] + [tmp[-1]]
         output += [[
             '',
-            f'{location.type_name}(const String& path):',
+            f'{obj.typename}(const String& path):',
             init_list,
             '{',
             '}',
         ]]
-    elif is_store:
-        tmp = [f'Store(db, {make_string(key)})']
-        tmp += [f'{make_identifier(c)}(store)' for c in children]
+    elif isinstance(obj, Store):
+        tmp = [f'Store(db, {make_string(obj.name)})']
+        tmp += [f'{child.id}(*this)' for child in obj.children]
         init_list = [f'{x},' for x in tmp[:-1]] + [tmp[-1]]
         output += [[
             '',
-            f'{location.type_name}(ConfigDB::Database& db):',
+            f'{obj.typename}(ConfigDB::Database& db):',
             init_list,
             '{',
             '}',
         ]]
     else:
-        tmp = [f'Object(store, {make_string(location.path)})']
-        tmp += [f'{make_identifier(c)}(store)' for c in children]
+        tmp = [f'Object(store, {make_string(obj.path)})']
+        tmp += [f'{child.id}(store)' for child in obj.children]
         init_list = [f'{x},' for x in tmp[:-1]] + [tmp[-1]]
         output += [[
             '',
-            f'{location.type_name}(ConfigDB::{store_kind}::Store& store):',
+            f'{obj.typename}(ConfigDB::{obj.store.base_class}& store):',
             init_list,
             '{',
             '}',
         ]]
-    for key, value in config['properties'].items():
-        loc = JsonPath(location.nodes + [key], key)
+    for key, value in obj.properties.items():
         ptype = value['type']
         ctype = CPP_TYPENAMES.get(ptype)
         if not ctype:
-            print(f'*** "{loc.path}": {ptype} type not yet implemented.')
+            print(f'*** "{obj.path}": {ptype} type not yet implemented.')
             continue
         if ctype == '-':
             continue
-        output += [parse_basic(loc, value, ctype)]
+        output += [parse_basic(obj.path, key, ctype)]
     output += [
         '',
-        [f'{make_typename(c)} {make_identifier(c)};' for c in children],
+        [f'{child.typename} {child.id};' for child in obj.children],
         '};'
     ]
     return output
 
 
-def parse_array(location: JsonPath, config: dict) -> list:
+def parse_array(config: dict) -> list:
     # TODO
     return []
 
 
-def parse_basic(location: JsonPath, config: dict, value_type: str) -> list:
+def parse_basic(path: str, value_name: str, value_type: str) -> list:
+    value_path = join_path(path, value_name)
     return [
         '',
-        f'{value_type} get{location.type_name}()',
+        f'{value_type} get{make_typename(value_name)}()',
         '{',
         [
-            f'return getValue<{value_type}>({make_string(location.name)});',
+            f'return getValue<{value_type}>({make_string(value_path)});',
         ],
         '}'
         '',
-        f'bool set{location.type_name}(const {value_type}& value)',
+        f'bool set{make_typename(value_name)}(const {value_type}& value)',
         '{',
         [
-            f'return setValue({make_string(location.name)}, value);'
+            f'return setValue({make_string(value_path)}, value);'
         ],
         '}'
     ]
@@ -169,24 +234,13 @@ def main():
 
     args = parser.parse_args()
 
-    with open(args.cfgfile, 'r') as f:
-        config = json.load(f)
-    # Top-level object is implicitly a database
-    config['database'] = True
+    db = load_config(args.cfgfile)
 
     # Find out what stores are in use
-    stores = set()
-    def find_stores(cfg: dict):
-        kind = cfg.get('store')
-        if kind:
-            stores.add(kind)
-        for x in cfg.values():
-            if isinstance(x, dict):
-                find_stores(x)
-    find_stores(config)    
-    print(f'stores: {stores}')
+    store_namespaces = set([child.store.namespace for child in db.children])
+    store_namespaces.add(db.store.namespace)
+    print(f'stores: {store_namespaces}')
 
-    name = os.path.splitext(os.path.basename(args.cfgfile))[0]
     output = [
         '/****',
         ' *',
@@ -194,11 +248,11 @@ def main():
         ' *',
         ' ****/',
         '',
-        *[f'#include <ConfigDB/{make_typename(kind)}.h>' for kind in stores],
-        *parse_object(JsonPath([name], name), None, config)
+        *[f'#include <ConfigDB/{typ}.h>' for typ in store_namespaces],
+        *parse_object(db)
     ]
 
-    filename = os.path.join(args.outdir, f'{name}.h')
+    filename = os.path.join(args.outdir, f'{db.name}.h')
     os.makedirs(args.outdir, exist_ok=True)
     with open(filename, 'w') as f:
         def dump_output(items: list, indent: str):
@@ -211,6 +265,11 @@ def main():
                     f.write('\n')
         dump_output(output, '')
 
+def test_load() -> dict:
+    with open('../samples/Basic_Config/basic-config.cfgdb') as f:
+        config = json.load(f)
+    parse_config('basic-config', config)
+    return config
 
 if __name__ == '__main__':
     try:
