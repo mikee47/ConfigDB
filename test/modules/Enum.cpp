e9510828df5722faf49b95a81bd42dd7f95a5078
mikee47 <mike@sillyhouse.net>
2026-02-06 20:43:16 +0000

    Add tests for int, string enums (raw, without ctype overrides)

--- test/modules/Enum.cpp
+++ test/modules/Enum.cpp
@@ -76,6 +76,28 @@ public:
 			REQUIRE(!range.contains(badColor));
 			REQUIRE_EQ(range.clip(badColor), Color::blue);
 		}
+
+		/*
+		 * RAW enums without ctype override
+		 */
+		TEST_CASE("Raw enums")
+		{
+			// These properties contain a value index, not the value itself
+			auto numIndex = root.getNum();
+			int num = TestConfigEnum::numType.values()[numIndex];
+#ifdef SMING_RELEASE
+			REQUIRE_EQ(numIndex, 4);
+			REQUIRE_EQ(num, 25);
+#else
+			REQUIRE_EQ(numIndex, 5);
+			REQUIRE_EQ(num, 45);
+#endif
+
+			auto wordIndex = root.getWord();
+			String word = TestConfigEnum::wordType.values()[wordIndex];
+			REQUIRE_EQ(wordIndex, 2);
+			REQUIRE_EQ(word, "brown");
+		}
 	}
 };
 
